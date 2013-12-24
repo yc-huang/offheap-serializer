b51f72b7207511fd644699f03540440c4a506be7
YC Huang <ychuang.cn@gmail.com>
2013-12-24 17:29:28 +0800

    add COMPACT flag to StringSerializer

--- off-heap-serializer-core/src/main/java/org/yong3/offheap/serialization/serializer/StringSerializer.java
+++ off-heap-serializer-core/src/main/java/org/yong3/offheap/serialization/serializer/StringSerializer.java
@@ -2,48 +2,31 @@ package org.yong3.offheap.serialization.serializer;
 
 import org.yong3.offheap.serialization.SerializerFactory;
 
-
 public class StringSerializer extends BaseObjectSerializer<String> {
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	CharArraySerializer charArraySerializer = (CharArraySerializer) SerializerFactory.get(char[].class);
+	final static boolean COMPACT = false;
+	CharArraySerializer charArraySerializer = (CharArraySerializer) SerializerFactory
+			.get(char[].class);
+	ByteArraySerializer byteArraySerializer = (ByteArraySerializer) SerializerFactory
+			.get(byte[].class);
 
 	public int getNotNullObjectSize(String instance) {
-		return charArraySerializer.getNotNullObjectSize(instance.toCharArray());
+		return (COMPACT) ? byteArraySerializer.getNotNullObjectSize(instance
+				.getBytes()) : charArraySerializer
+				.getNotNullObjectSize(instance.toCharArray());
 	}
 
 	@Override
 	protected int writeNotNullObjectSize(long address, String instance) {
-		return charArraySerializer.writeNotNullObjectSize(address,
-				instance.toCharArray());
-//		long addr = address;
-//		char[] chars = instance.toCharArray();
-//		allocator.putInt(addr, chars.length);
-//		addr += allocator.sizeOf(chars.length);
-//		for(char c : chars){
-//			allocator.putChar(addr, c);
-//			addr += allocator.sizeOf(c);
-//		}
-//		
-//		return (int) (addr - address);
-		
-		//return allocator.putCharArray(address, instance.toCharArray());
+		return (COMPACT) ? byteArraySerializer.writeNotNullObjectSize(address,
+				instance.getBytes()) : charArraySerializer
+				.writeNotNullObjectSize(address, instance.toCharArray());
 	}
 
 	@Override
 	protected String readNotNullObject(long address, String instance) {
-		char[] chars = null;
-		chars = charArraySerializer.readNotNullObject(address, null);
-//		return new String(chars);
-//		long addr = address;
-//		chars = new char[allocator.getInt(addr)];
-//		addr += allocator.sizeOf(chars.length);
-//		for(int i = 0; i < chars.length; i++){
-//			chars[i] = allocator.getChar(addr);
-//			addr += allocator.sizeOf(chars[i]);
-//		}
-//		chars = allocator.getCharArray(address);
-		
-		return new String(chars);
+		return (COMPACT) ? new String(byteArraySerializer.readNotNullObject(
+				address, null)) : new String(
+				charArraySerializer.readNotNullObject(address, null));
 	}
 
 	@Override
